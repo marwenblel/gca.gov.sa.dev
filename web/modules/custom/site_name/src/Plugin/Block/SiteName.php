b3bea1dc7bb64dcb2855a3439fbed95994f8d877
marwenblel <marwen.blel@gmail.com>
2024-05-25 03:20:31 +0200

    Creating new custom theme named GCA, then initializing css and some others initials functionnalities.

---
+++ web/modules/custom/site_name/src/Plugin/Block/SiteName.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\site_name\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Site Name' Block.
+ *
+ * @Block(
+ *   id = "site_name",
+ *   admin_label = @Translation("Site Name"),
+ * )
+ */
+class SiteName extends BlockBase {
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+        $site_name = \Drupal::config('system.site')->get('name');
+        return [
+            '#markup' => '<h3>' . $this->t('@site_name', ['@site_name' => $site_name]) . '</h3>',
+            '#allowed_tags' => ['h3'],
+        ];
+    }
+
+}
